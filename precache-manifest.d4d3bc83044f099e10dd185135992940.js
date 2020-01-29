aedd57c3e0b631f024cca1bcc1c97f737eae847d
imamfahrezi <imamfahrezi24@gmail.com>
2020-01-29 21:52:20 +0700

    Updates

---
+++ precache-manifest.d4d3bc83044f099e10dd185135992940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a29bfcafcb29bbe43aaaa02482d2e9c",
+    "url": "/kuliner-cikuda/index.html"
+  },
+  {
+    "revision": "4066045c5aa19e986aa0",
+    "url": "/kuliner-cikuda/static/css/main.f9086cdd.chunk.css"
+  },
+  {
+    "revision": "e80d4a8d3dc43356592a",
+    "url": "/kuliner-cikuda/static/js/2.82b46302.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/kuliner-cikuda/static/js/2.82b46302.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4066045c5aa19e986aa0",
+    "url": "/kuliner-cikuda/static/js/main.8e9a8b99.chunk.js"
+  },
+  {
+    "revision": "6abf7fca63eafe7116a2",
+    "url": "/kuliner-cikuda/static/js/runtime-main.05e20914.js"
+  },
+  {
+    "revision": "1a85ce208cf590ab694935b5f4d84bcf",
+    "url": "/kuliner-cikuda/static/media/aneka-makanan.1a85ce20.png"
+  }
+]);
